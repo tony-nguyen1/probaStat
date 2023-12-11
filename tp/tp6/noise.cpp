0708d071f08e9e4a54853f26b96b180f56a46b2b
tony-nguyen1 <nguyentoony@gmail.com>
2023-12-11 16:29:18 +0100

    sauvegarde

--- tp/tp6/noise.cpp
+++ tp/tp6/noise.cpp
@@ -19,8 +19,8 @@ int main(int argc, char* argv[])
     }
 
     sscanf (argv[1],"%s",cNomImgIn1Lue);
-    sscanf (argv[1],"%s",cNomImgIn2Lue);
-    sscanf (argv[1],"%s",cNomImgOutLue);
+    sscanf (argv[2],"%s",cNomImgIn2Lue);
+    sscanf (argv[3],"%s",cNomImgOutLue);
 
     OCTET *ImgIn1, *ImgIn2, *ImgOut;
     lire_nb_lignes_colonnes_image_pgm(cNomImgIn1Lue, &nH, &nW);
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
     allocation_tableau(ImgOut, OCTET, nTaille);
     lire_image_pgm(cNomImgIn1Lue, ImgIn1, nH * nW);
     lire_image_pgm(cNomImgIn2Lue, ImgIn1, nH * nW);
-    lire_image_pgm(cNomImgOutLue, ImgIn1, nH * nW);
+    // lire_image_pgm(cNomImgOutLue, ImgIn1, nH * nW);
 
 
     for (int i=0; i < nH; i++) {
@@ -39,28 +39,15 @@ int main(int argc, char* argv[])
             int diff = 0;
 
             diff = ImgIn1[i*nW+j] - ImgIn2[i*nW+j];
-            // if (dif < )
-            // diff
+            diff = diff / 2;
+            diff = diff + 128;
 
             ImgOut[i*nW+j] = diff+128;
         }
     }
 
-    free(ImgIn1);free(ImgIn2);
+    ecrire_image_pgm(cNomImgOutLue, ImgOut,  nH, nW);
+    free(ImgIn1);free(ImgIn2);free(ImgOut);
 
     return 0;
-}
-
-void fctGauss(float mu, float sigma) {
-    for (int i = 0; i < 256; i++) {
-        float f, numerateur2, denominateur1, denominateur2;
-        denominateur1 = (float)sigma*sqrtf(2*M_PI);
-        //printf("test1 %f\n", denominateur1);
-        numerateur2 = powf((float)(i-mu),2);
-        //printf("test2 %f\n", numerateur2);
-        denominateur2 = (float)(2*sigma*sigma);
-        //printf("test3 %f\n", denominateur2);
-        f = (float)(1/denominateur1)*expf(-numerateur2/denominateur2);
-        //printf("%d %f\n", i, f);
-    }
 }
